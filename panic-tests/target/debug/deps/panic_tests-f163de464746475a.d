dd049cf32a65721956d6951d840a7e164f172910
kseniailina <kse1920@yandex.ru>
2020-10-20 13:51:02 +0300

    add tests projects

---
+++ panic-tests/target/debug/deps/panic_tests-f163de464746475a.d
@@ -0,0 +1,5 @@
+C:\Users\ksenia.ilina\temp\rust-project\panic-tests\target\debug\deps\panic_tests-f163de464746475a.exe: src\main.rs
+
+C:\Users\ksenia.ilina\temp\rust-project\panic-tests\target\debug\deps\panic_tests-f163de464746475a.d: src\main.rs
+
+src\main.rs:
---
+++ tests/target/debug/deps/tests-79c2c0ed6d05f0ee.d
@@ -0,0 +1,5 @@
+C:\Users\ksenia.ilina\temp\rust-project\tests\target\debug\deps\tests-79c2c0ed6d05f0ee.exe: src\main.rs
+
+C:\Users\ksenia.ilina\temp\rust-project\tests\target\debug\deps\tests-79c2c0ed6d05f0ee.d: src\main.rs
+
+src\main.rs:
